dbf464fd541556c73ae635464e4999ba4cabaefc
Jasswin-Jass <jasswinjass@gmail.com>
2025-05-24 14:58:05 +0530

    Added web files

---
+++ index.js
@@ -0,0 +1,13 @@
+var sidenav = document.querySelector(".side-navbar")
+
+function opennavbar()
+{
+    sidenav.style.left = "0"
+}
+
+function closenavbar()
+{
+    sidenav.style.left = "-50%"
+}
+
+// pica , char, arcius, mew , blezigen 
